439298263a837f64f18031abc0787b6f6761123d
jython234 <jython234@users.noreply.github.com>
2016-10-30 14:10:44 -0500

    Event system!

---
+++ source/mango_engine/event/core.d
@@ -0,0 +1,137 @@
+/*
+ *  BSD 3-Clause License
+ *  
+ *  Copyright (c) 2016, Mango-Engine Team
+ *  All rights reserved.
+ *  
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *  
+ *  * Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *  
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *  
+ *  * Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *  
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ *  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ *  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+module mango_engine.event.core;
+
+import mango_engine.game;
+import mango_engine.util;
+
+import mango_stl.collections;
+
+import std.conv;
+
+/// Represents a callable event with properties.
+abstract class Event {
+
+}
+
+/// Event that is fired each tick.
+class TickEvent : Event {
+    immutable ulong currentTick;
+
+    this(in ulong currentTick) @safe nothrow pure {
+        this.currentTick = currentTick;
+    }
+}
+
+/// Event that is fired when the engine begins to clean up.
+class EngineCleanupEvent : Event {
+
+}
+
+/// Alias for a delegate used for an event hook.
+alias HookDelegate = void delegate(Event evt) @system;
+
+/// Represents a hook that is called for a specific event.
+struct EventHook {
+    /// The function delegate to be ran.
+    immutable HookDelegate hook;
+    /// If the delegate can be ran in a separate worker thread.
+    immutable bool runAsync = true;
+}
+
+class EventManager {
+	__gshared {
+		private ThreadPool pool;
+		private GameManager game;
+		
+		private Queue!Event eventQueue;
+	}
+	
+	private shared SyncLock hookLock;
+	private shared EventHook[][string] hooks;
+	
+	this(GameManager game) @trusted {
+		import core.cpuid : coresPerCPU;
+		
+		this.game = game;
+		this.game.logger.logInfo("This CPU has " ~ to!string(coresPerCPU()) ~ " cores avaliable. Assigning one worker thread to each.");
+		this.pool = new ThreadPool(coresPerCPU());
+		this.eventQueue = new Queue!Event();
+		
+		this.hookLock = new SyncLock();
+		
+		this.registerEventHook(EngineCleanupEvent.classinfo.name, EventHook(&this.stop, false));
+	}
+	
+	
+	private void stop(Event evt) @trusted {
+        this.pool.stopImmediate();
+    }
+
+    /++
+        Adds an event to the firing queue. The event
+        will be fired on the next EventManager update
+        pass.
+    +/
+    void fireEvent(Event event) @trusted {
+        this.eventQueue.add(event); // Queue is thread-safe
+    }
+
+    void registerEventHook(in string eventType, EventHook hook) @trusted {
+        synchronized(this.hookLock) {
+            this.hooks[eventType] ~= cast(shared) hook;
+        }
+    }
+
+    /// Update function called by GameManager
+    void update(size_t limit = 25) @trusted {
+    	if(this.eventQueue.isEmpty()) return;
+    	
+    	if(limit == 0) {
+    		limit = size_t.max;
+    	}
+    	while(!this.eventQueue.isEmpty() && limit-- > 0) {
+    		Event event = this.eventQueue.pop();
+    		if(event.classinfo.name in this.hooks) {
+    			foreach(EventHook hook; this.hooks[event.classinfo.name]) {
+    				if(hook.runAsync) { // Check if we can run this in a worker
+                        pool.submitWork(() {
+                            hook.hook(cast(Event) event);
+                        });
+                    } else {
+                        hook.hook(cast(Event) event);
+                    }
+    			}
+    		}
+    	}
+    }
+}
--- source/mango_engine/game.d
+++ source/mango_engine/game.d
@@ -1,7 +1,39 @@
+/*
+ *  BSD 3-Clause License
+ *  
+ *  Copyright (c) 2016, Mango-Engine Team
+ *  All rights reserved.
+ *  
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *  
+ *  * Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *  
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *  
+ *  * Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *  
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ *  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ *  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 module mango_engine.game;
 
 import mango_engine.mango;
 import mango_engine.logging;
+import mango_engine.event.core;
 import mango_engine.graphics.window;
 import mango_engine.graphics.renderer;
 
@@ -29,7 +61,7 @@ class GameManagerFactory {
         this.renderer = renderer;
     }
 
-    GameManager build() @safe nothrow {
+    GameManager build() @safe {
         return new GameManager(
             new ConsoleLogger("Game"),
             window,
@@ -44,25 +76,31 @@ class GameManager {
 
     private shared Window _window;
     private shared Renderer _renderer;
+    
+    private shared EventManager _eventManager;
     private shared Logger _logger;
 
     @property Window window() @trusted nothrow { return cast(Window) _window; }
     @property Renderer renderer() @trusted nothrow { return cast(Renderer) _renderer; }
+    @property EventManager eventManager() @trusted nothrow { return cast(EventManager) _eventManager; }
     @property Logger logger() @trusted nothrow { return cast(Logger) _logger; }
 
     private shared bool running = false;
 
-    package this(Logger logger, Window window, Renderer renderer, BackendType type) @trusted nothrow {
+    package this(Logger logger, Window window, Renderer renderer, BackendType type) @trusted {
         this.backendType = type;
 
+		this._logger = cast(shared) logger;
         this._renderer = cast(shared) renderer;
         this._window = cast(shared) window;
+        
+        this._eventManager = cast(shared) new EventManager(this);
     }
 
-    void run() @safe {
-        enforce(!running, new Exception("Game is already running!"));
+    void run() @trusted {
+        enforce(!this.running, new Exception("Game is already running!"));
 
-        running = true;
+        this.running = true;
 
         ulong ticks = 0;
 
@@ -70,10 +108,31 @@ class GameManager {
         long time = 1000 / fps;
         StopWatch sw = StopWatch();
 
-        logger.logDebug("Entering main loop.");
+        this.logger.logDebug("Entering main loop.");
 
         do {
-            
-        } while(running);
+			sw.reset();
+            sw.start();
+
+            TickEvent te = new TickEvent(ticks);
+
+            this.eventManager.fireEvent(te);
+            this.eventManager.update();
+
+            sw.stop();
+            if(sw.peek.msecs < time) {
+                Thread.sleep((time - sw.peek.msecs).msecs);
+            } else {
+                version(mango_warnOvertime) {
+                    this.logger.logWarn("Can't keep up! (" ~ to!string(sw.peek.msecs) ~ " > " ~ to!string(time) ~ ")");
+                }
+            }
+            ticks++;
+        } while(this.running);
+        
+        this.logger.logDebug("Cleaning up...");
+        
+        this.eventManager.fireEvent(new EngineCleanupEvent());
+        this.eventManager.update(0);
     }
 }
