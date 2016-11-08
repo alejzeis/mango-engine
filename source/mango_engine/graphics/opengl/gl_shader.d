839fff18e7658ab1bd97b9c21d32af9be9367dd3
jython234 <jython234@users.noreply.github.com>
2016-11-08 09:14:24 -0600

    More shader work

--- source/mango_engine/graphics/opengl/gl_shader.d
+++ source/mango_engine/graphics/opengl/gl_shader.d
@@ -4,7 +4,21 @@ version(mango_GLBackend) {
     import mango_engine.game;
     import mango_engine.graphics.shader;
 
+    import blocksound.util : toCString;
+
     import derelict.opengl3.gl3;
+
+    /// Gets the OpenGL constant for a specific ShaderType.
+    GLuint shaderTypeToGL(ShaderType type) @safe nothrow {
+        final switch(type) {
+            case ShaderType.SHADER_VERTEX:
+                return GL_VERTEX_SHADER;
+            case ShaderType.SHADER_FRAGMENT:
+                return GL_FRAGMENT_SHADER;
+            case ShaderType.SHADER_COMPUTE:
+                return GL_COMPUTE_SHADER;
+        }
+    }
 
     class GLShaderProgram : ShaderProgram {
         __gshared package GLuint programId;
@@ -57,25 +71,30 @@ version(mango_GLBackend) {
     }
 
     class GLShader : Shader {
+        package immutable string source;
         __gshared package GLuint shaderId;
 
         this(GameManager game, in string source, in ShaderType type) @safe {
             super(game, source, type);
+            this.source = source;
 
             game.renderer.submitOperation(&this.setup);
         }
 
-        private void setup() @system nothrow {
+        private void setup() @system {
+            this.shaderId = glCreateShader(shaderTypeToGL(type));
             
+            char* source = toCString(this.source);
+            glShaderSource(this.shaderId, 1, &source, null);
         }
 
         override {
             protected void onShaderAdd() @system {
-
+                glCompileShader(this.shaderId);
             }
 
             protected void cleanup() @system {
-
+                glDeleteShader(this.shaderId);
             }
         }
     }
